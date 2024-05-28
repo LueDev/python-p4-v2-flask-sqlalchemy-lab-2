e1f78d5470ab143db023ce5d1db927e6a223edcb
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-05-28 17:26:59 -0400

    This was a wonderful challenge. I learned how to properly create relationships, back populating against models and their attributes, association proxies, apply serialize rules of exclusion, and update the migration after the addition of the Reviews model table.

--- server/models.py
+++ server/models.py
@@ -10,23 +10,56 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+'''
+While we store the relationship in just one place in the database, we often need to access and update both sides of the relationship within our application. We will establish a bidirectional relationship between two models (one-to-many and many-to-one) by making the following updates:
 
-class Customer(db.Model):
+Add a foreign key column to the model on the "many" or "belongs to" side to store the one-to-many relationship.
+Add a relationship to the model on the "one" side to reference a list of associated objects from the "many" side.
+Add a reciprocal relationship to the model on the "many" side and connect both relationships using the back_populates property.
+'''
+
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
+    
+    #MUST pass a tuple for serialize_rules and only
+    serialize_rules = ('-reviews.customer',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    reviews = db.relationship('Review', back_populates='customer')
+    items = association_proxy('reviews', 'item')
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
-
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
+    
+    serialize_rules = ('-reviews.item',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship('Review', back_populates='item')
+    customers = association_proxy('reviews', 'customer')
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+    
+    serialize_rules = ('-customer.reviews', '-item.reviews')
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}, {self.customer} -- {self.item}>'
